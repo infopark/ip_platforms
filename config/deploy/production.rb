c2eaaabe66da795e5f2f919806c312022d6fb958
Thomas Witt <thomas.witt@infopark.de>
2011-01-17 17:24:39 +0100

    Corrected IP Address for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :web, 'IPVMW2'
-role :app, 'IPVMW2'
-role :db,  'IPVMW2', :primary => true
+role :web, '192.168.193.80'
+role :app, '192.168.193.80'
+role :db,  '192.168.193.80', :primary => true
 
 set :deploy_to, '/home/rails/deploy'
 set :user, 'rails'
