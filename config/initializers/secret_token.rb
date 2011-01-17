9ad7d3d0230f316cbc0c2ea6a3bcb982bfa831b3
Stefan Krause <stefan.krause@infopark.de>
2011-01-17 17:36:41 +0100

    mysql2, etc.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 IpPlatforms::Application.config.secret_token = 'e1ac6e5e4bd0dc7b2b55a8b64958b489552d46a6edea54578108acbb8c9cf92a717fa2355ece12a3447ebc9a72c829f51cf2b87e6c79f33959e1255f54586f31'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
 IpPlatforms::Application.config.session_store :cookie_store, :key => '_ip_platforms_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# IpPlatforms::Application.config.session_store :active_record_store
